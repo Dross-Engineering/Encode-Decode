b522bacbab5271d18c9537619fdd179d8038efe1
AmossT <168945718+Dross-Engineering@users.noreply.github.com>
2025-02-10 15:08:59 +1100

    Update Encode_Decode.py
    added comments and made the padding and new line character replacements more robust to prevent accidental encoding issues.

--- Encode_Decode.py
+++ Encode_Decode.py
@@ -1,12 +1,24 @@
-'''
-███████╗███╗   ██╗ ██████╗ ██████╗ ██████╗ ███████╗    ██████╗ ███████╗ ██████╗ ██████╗ ███████╗
-██╔════╝████╗  ██║██╔═══██╗██╔══██╗██╔══██╗██╔════╝    ██╔══██╗██╔════╝██╔═══██╗██╔══██╗██╔════╝
-█████╗  ██╔██╗ ██║██║   ██║██║  ██║██║  ██║█████╗      ██║  ██║█████╗  ██║   ██║██║  ██║█████╗
-██╔══╝  ██║╚██╗██║██║   ██║██║  ██║██║  ██║██╔══╝      ██║  ██║██╔══╝  ██║   ██║██║  ██║██╔══╝
-███████╗██║ ╚████║╚██████╔╝██████╔╝██████╔╝███████╗    ██████╔╝███████╗╚██████╔╝██████╔╝███████╗
-╚══════╝╚═╝  ╚═══╝ ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝    ╚═════╝ ╚══════╝ ╚═════╝ ╚═════╝ ╚══════╝
-Created by AmossT 2024
-'''
+"""
+░▒▓████████▓▒░▒▓███████▓▒░ ░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓███████▓▒░░▒▓████████▓▒░
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░
+░▒▓██████▓▒░ ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓██████▓▒░
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░
+░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓███████▓▒░░▒▓████████▓▒░
+
+
+░▒▓███████▓▒░░▒▓████████▓▒░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓███████▓▒░░▒▓████████▓▒░
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░     ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░
+░▒▓█▓▒░░▒▓█▓▒░▒▓██████▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓██████▓▒░
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░     ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░
+░▒▓███████▓▒░░▒▓████████▓▒░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓███████▓▒░░▒▓████████▓▒░
+
+Created by Hugo T 2024
+"""
+
 import random
 import tkinter as tk
 import re
@@ -17,33 +29,39 @@ import sys
 import secrets
 
 def encode(text, seed):
-    seed = ''.join(str(ord(char)) for char in seed)
-    rand_offset = secrets.randbelow(35)/100
-    pad_length1 = 7+secrets.randbelow(25+int(round((rand_offset+0.09)*len(text))))
-    pad_length2 = 5+secrets.randbelow(30+int(round((rand_offset+0.08)*len(text))))
-    padded_text = pad_length1 * "0" + text + pad_length2 * "0"
-    random.seed(seed)
-    encoded_text = ''
+    seed = ''.join(str(ord(char)) for char in seed)     # Converts seed to a numeric code using HEX
+    seed = str(int(seed)//11)                           # Divides seed by 11 to equalise integer distribution
+    rand_offset = secrets.randbelow(25)/100             # Creates a random offset percentage below 35%
+    pad_length1 = 7+secrets.randbelow(11+int(round((rand_offset+0.03)*len(text))))  # Creates pad 1
+    pad_length2 = 5+secrets.randbelow(16+int(round((rand_offset+0.05)*len(text))))  # creates pad 2
+    padded_text_raw = pad_length1 * "`/" + text + pad_length2 * "`/"  # pads text with '  '
+    random.seed(seed)               # Sets random seed for random module. Key for reversing the encoding
 
-    for char in padded_text:
+    padded_text = ''
+    for char in padded_text_raw:    # Converts new lines (\n) characters to ``@ so they survive the encoding step
         if char == '\n':
-            char = '`'
-        encoded_char = ord(char) + random.randint(0, 94)
-        if encoded_char > 126:
+            char = '`%'
+        padded_text += char
+
+    encoded_text = ''
+    for char in padded_text:        # Randomly maps each char to another valid char (Unicode 94-126) based on
+        encoded_char = ord(char) + random.randint(0, 94)  # Random mapping changes for each char to prevent
+        if encoded_char > 126:                                  # patterns being used to crack the cypher
             encoded_char -= 94
         encoded_text += chr(encoded_char)
 
-    random.seed(seed * 3)
-    encoded_text = list(encoded_text)
-    random.shuffle(encoded_text)
+    random.seed(str(int(seed) // 11))   # changing the random seed by dividing by a prime number
+
+    encoded_text = list(encoded_text)   # Converting to list to allow for shuffling
+    random.shuffle(encoded_text)        # Encoded text is shuffled, this will remove any potential remaining pattern
     shuffled_text = ''.join(encoded_text)
     return shuffled_text
 
 
 def decode(encoded_text, seed):
     seed = ''.join(str(ord(char)) for char in seed)
-
-    random.seed(seed * 3)
+    seed = str(int(seed)//11)
+    random.seed(str(int(seed) // 11))
     original_order = list(range(len(encoded_text)))
     shuffled_order = original_order[:]
     random.shuffle(shuffled_order)
@@ -56,8 +74,8 @@ def decode(encoded_text, seed):
         if decoded_char < 32:
             decoded_char += 94
         decoded_text += chr(decoded_char)
-    decoded_text = decoded_text.replace('`', '\n')
-    return decoded_text.strip("0")
+    decoded_text = decoded_text.replace('`%', '\n')
+    return decoded_text.strip("`/")
 
 
 def on_encode_button():
@@ -121,6 +139,8 @@ def copy_seed():
     root.update()  # Keeps the clipboard content after the script ends
 
 
+# Building the GUI ----------------------------------------------------
+
 root = tk.Tk()
 root.title("ENCODE/DECODE")     # Setting gui title
 
@@ -182,4 +202,3 @@ pad_frame.grid_columnconfigure(0, weight=1)  # Main column
 
 root.geometry("450x400")
 root.mainloop()
-
